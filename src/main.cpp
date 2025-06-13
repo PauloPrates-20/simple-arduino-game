aa195918067e07dd2a1b14e4b780a9cf213caa01
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-13 13:19:29 -0300

    feat: drawn dinosaur sprite

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,9 @@
 #include "config/display.h"
 
 LiquidCrystal lcd(RS, ENABLE, D0, D1, D2, D3, D4, D5, D6, D7);
-uint8_t bell[8]  = {0x4,0xe,0xe,0xe,0x1f,0x0,0x4};
-uint8_t note[8]  = {0x2,0x3,0x2,0xe,0x1e,0xc,0x0};
-uint8_t clock[8] = {0x0,0xe,0x15,0x17,0x11,0xe,0x0};
-uint8_t heart[8] = {0x0,0xa,0x1f,0x1f,0xe,0x4,0x0};
-uint8_t duck[8]  = {0x0,0xc,0x1d,0xf,0xf,0x6,0x0};
-uint8_t check[8] = {0x0,0x1,0x3,0x16,0x1c,0x8,0x0};
-uint8_t cross[8] = {0x0,0x1b,0xe,0x4,0xe,0x1b,0x0};
-uint8_t retarrow[8] = {	0x1,0x1,0x5,0x9,0x1f,0x8,0x4};
+uint8_t dinossaur[8] = {0b00000, 0b00111, 0b00111, 0b10110, 0b11111, 0b01010, 0b01010, 0b00000};
 
-void displayKeyCodes(void);
+void drawDino();
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,32 +14,17 @@ void setup() {
   analogWrite(CONTRAST_PIN, CONTRAST);
   lcd.begin(20,4);
   lcd.clear();
-  lcd.createChar(0, bell);
-  lcd.createChar(1, note);
-  lcd.createChar(2, clock);
-  lcd.createChar(3, heart);
-  lcd.createChar(4, duck);
-  lcd.createChar(5, check);
-  lcd.createChar(6, cross);
-  lcd.createChar(7, retarrow);
+  lcd.createChar(0, dinossaur);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  displayKeyCodes();
+  drawDino();
 }
 
-void displayKeyCodes(void) {
-  uint8_t i = 0;
-  while (1) {
-    lcd.clear();
-    lcd.print("Codes 0x"); lcd.print(i, HEX);
-    lcd.print("-0x"); lcd.print(i+16, HEX);
-    lcd.setCursor(0, 1);
-    for (int j=0; j<16; j++) {
-      lcd.write(i+j);
-    }
-    i+=16;
-    
-    delay(10000);
-  }}    
+void drawDino(void) {
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(char(0));
+  delay(1000);
+}
