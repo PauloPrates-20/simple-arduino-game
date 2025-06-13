f1709e3ecedf36a74b135bf10908fd2c8db026c2
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-13 12:58:44 -0300

    init: project setup

---
+++ src/config/display.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// LCD data pins
+#define D0 2
+#define D1 3
+#define D2 4
+#define D3 5
+#define D4 6
+#define D5 7
+#define D6 8
+#define D7 9
+
+// LCD control pins
+#define ENABLE 12
+#define RS 11
+#define CONTRAST_PIN 10
+#define CONTRAST 20
---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include "config/display.h"
+
+LiquidCrystal lcd(RS, ENABLE, D0, D1, D2, D3, D4, D5, D6, D7);
+uint8_t bell[8]  = {0x4,0xe,0xe,0xe,0x1f,0x0,0x4};
+uint8_t note[8]  = {0x2,0x3,0x2,0xe,0x1e,0xc,0x0};
+uint8_t clock[8] = {0x0,0xe,0x15,0x17,0x11,0xe,0x0};
+uint8_t heart[8] = {0x0,0xa,0x1f,0x1f,0xe,0x4,0x0};
+uint8_t duck[8]  = {0x0,0xc,0x1d,0xf,0xf,0x6,0x0};
+uint8_t check[8] = {0x0,0x1,0x3,0x16,0x1c,0x8,0x0};
+uint8_t cross[8] = {0x0,0x1b,0xe,0x4,0xe,0x1b,0x0};
+uint8_t retarrow[8] = {	0x1,0x1,0x5,0x9,0x1f,0x8,0x4};
+
+void displayKeyCodes(void);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(CONTRAST_PIN, OUTPUT);
+  analogWrite(CONTRAST_PIN, CONTRAST);
+  lcd.begin(20,4);
+  lcd.clear();
+  lcd.createChar(0, bell);
+  lcd.createChar(1, note);
+  lcd.createChar(2, clock);
+  lcd.createChar(3, heart);
+  lcd.createChar(4, duck);
+  lcd.createChar(5, check);
+  lcd.createChar(6, cross);
+  lcd.createChar(7, retarrow);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  displayKeyCodes();
+}
+
+void displayKeyCodes(void) {
+  uint8_t i = 0;
+  while (1) {
+    lcd.clear();
+    lcd.print("Codes 0x"); lcd.print(i, HEX);
+    lcd.print("-0x"); lcd.print(i+16, HEX);
+    lcd.setCursor(0, 1);
+    for (int j=0; j<16; j++) {
+      lcd.write(i+j);
+    }
+    i+=16;
+    
+    delay(10000);
+  }}    
