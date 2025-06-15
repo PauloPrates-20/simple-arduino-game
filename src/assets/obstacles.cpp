412343909a736fdeeccb33d66cbb906d45255bf4
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-15 20:50:41 -0300

    fix: sprites do passáro

--- src/assets/obstacles.cpp
+++ src/assets/obstacles.cpp
@@ -12,7 +12,7 @@ uint8_t cacti_small[8] = {0b00000, 0b00000, 0b00100, 0b00101, 0b10111, 0b11100, 
 
 // bird
 uint8_t bird_down[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b00111, 0b00110, 0b00100, 0b00000};
-uint8_t bird_up[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b11111, 0b00000, 0b00000};
+uint8_t bird_up[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b01111, 0b00000, 0b00000};
 
 void animateBird(Obstacle** birds, size_t bird_count) {
   if (millis() - _last_sprite >= ANIMATION_TIME) {
