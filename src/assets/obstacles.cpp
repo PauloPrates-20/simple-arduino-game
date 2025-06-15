d92052dd314b1fb1998aa338a857249ffa373099
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-15 20:48:53 -0300

    fix: sprites dos obstáculos

--- src/assets/obstacles.cpp
+++ src/assets/obstacles.cpp
@@ -7,12 +7,12 @@ static unsigned long _last_move = 0;
 static unsigned long _last_spawn = 0;
 
 // cacti
-uint8_t cacti_large[8] = {0b00100, 0b00101, 0b10101, 0b10101, 0b10111, 0b11100, 0b00100};
-uint8_t cacti_small[8] = {0b00000, 0b00000, 0b00100, 0b00101, 0b10111, 0b11100, 0b00100};
+uint8_t cacti_large[8] = {0b00100, 0b00101, 0b10101, 0b10101, 0b10111, 0b11100, 0b00100, 0b00100};
+uint8_t cacti_small[8] = {0b00000, 0b00000, 0b00100, 0b00101, 0b10111, 0b11100, 0b00100, 0b00100};
 
 // bird
 uint8_t bird_down[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b00111, 0b00110, 0b00100, 0b00000};
-uint8_t bird_up[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b00000, 0b00000, 0b00000};
+uint8_t bird_up[8] = {0b00000, 0b00100, 0b01100, 0b11110, 0b11111, 0b00000, 0b00000};
 
 void animateBird(Obstacle** birds, size_t bird_count) {
   if (millis() - _last_sprite >= ANIMATION_TIME) {
