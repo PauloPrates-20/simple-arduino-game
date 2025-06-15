a955d4da66271a57dcb60f8f2da710d222be39b6
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-15 14:21:51 -0300

    chore: defined obstacle memory locations

--- src/assets/obstacles.h
+++ src/assets/obstacles.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <stdint.h>
 
+#define MEM_OBS_CACTI_LARGE 3
+#define MEM_OBS_CACTI_SMALL 4
+#define MEM_OBS_BIRD 5
+
 // cacti
 extern uint8_t cacti_large[8];
 extern uint8_t cacti_small[8];
